09952e3a15cc9c27fe0defc1c9ab36bd4956fe2a
MasterCaio <caiofelipe147@gmail.com>
2016-04-02 15:41:47 -0300

    corrigindo merdas

---
+++ RailsDojo/db/migrate/20160402184126_create_usuarios.rb
@@ -0,0 +1,10 @@
+class CreateUsuarios < ActiveRecord::Migration
+  def change
+    create_table :usuarios do |t|
+      t.text :email
+      t.string :senha
+
+      t.timestamps null: false
+    end
+  end
+end
